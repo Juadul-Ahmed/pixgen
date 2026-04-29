4fe85db90b71eed7876241849b8a76336d11741e
juadul <parallaxhub@gmail.com>
2026-04-29 16:10:28 +0800

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import Footer from "@/components/Footer";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const OutfitFont = Outfit({
   subsets: ["latin"],
 });
 
@@ -20,9 +17,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${OutfitFont.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+      <NavBar/>
+      {children}
+      <Footer/>
+      </body>
     </html>
   );
 }
